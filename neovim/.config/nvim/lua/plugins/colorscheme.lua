3e0fc3762e7b82dc4d373f474b8950ffd9c8936b
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2024-06-14 14:46:29 +0530

    merge into colorscheme

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,10 @@
-return { -- You can easily change to a different colorscheme.
+return {
+  {
+    'catppuccin/nvim',
+    lazy = false,
+    name = 'catppuccin',
+  },
+  { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
@@ -9,9 +15,11 @@ return { -- You can easily change to a different colorscheme.
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-moon'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
-  }
+  },
+}
+
